ec4f06163d68d03e0ad32735896566e4169dd973
ASER1989 <aser1989@sina.com>
2025-05-15 03:10:23 +0000

    update vite config

--- nebula-client/vite.config.ts
+++ nebula-client/vite.config.ts
@@ -1,42 +1,62 @@
 /** @type {import('vite').UserConfig} */
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-const path = require("path");
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import compression from 'vite-plugin-compression';
+
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  return {
-    root: "./",
-    base: "./",
-    resolve: {
-      alias: {
-        "@client": path.resolve(__dirname, "./src"),
-        "@nebula-note/ui": path.resolve(__dirname, "../nebula-ui/dist"),
-      },
-    },
-    mode,
-    server: {
-      port: 3107,
-      strictPort: true,
-      proxy: {
-        "^/api": {
-          target: "http://localhost:3816/",
+    console.log('command:', command, mode);
+    return {
+        root: './',
+        base: './',
+        resolve: {
+            alias: {
+                '@client': path.resolve(__dirname, './src'),
+                '@nebula-note/ui': path.resolve(__dirname, '../nebula-ui/dist'),
+            },
         },
-      },
-      hmr: true,
-    },
-    plugins: [react()],
-    build: {
-      rollupOptions: {
-        input: ["./index.html"],
-      },
-      outDir: "dist",
-      sourcemap: command === "serve",
-      minify: command === "build",
-      esbuild: {
-        minify: true,
-        drop: ["console", "debugger"], 
-      },
-    },
-  };
+        mode,
+        server: {
+            port: 3107,
+            strictPort: true,
+            proxy: {
+                '^/api': {
+                    target: 'http://localhost:3816/',
+                },
+            },
+            hmr: true,
+        },
+        plugins: [
+            react(),
+            compression({
+                algorithm: 'gzip', 
+                ext: '.gz',
+                threshold: 1024,
+                deleteOriginFile: false,
+            }),
+            compression({
+                algorithm: 'brotliCompress',
+                ext: '.br',
+            }),
+        ],
+        build: {
+            rollupOptions: {
+                input: ['./index.html'],
+            },
+            outDir: 'dist',
+            sourcemap: command === 'serve',
+            minify: mode === 'production' ? 'terser' : false,
+            // terserOptions: {
+            //     format: {
+            //         comments: false,
+            //     },
+            //     compress: {
+            //         drop_console: true,
+            //         drop_debugger: true,
+            //     },
+            // },
+        },
+    };
 });
