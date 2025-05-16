ea6300ecdfb5d491e05187c692ddd090e1768930
ASER1989 <aser1989@sina.com>
2025-05-16 16:40:37 +0800

    Update usePermissions.ts

--- nebula-client/src/models/permissions/usePermissions.ts
+++ nebula-client/src/models/permissions/usePermissions.ts
@@ -4,7 +4,7 @@ import { useRedux } from '@client/store/hooks/useRedux';
 const REDUX_KEY = 'permissions';
 export const usePermissions = () => {
     const { state } = useRedux<Permission>(REDUX_KEY, {
-        isReadonly: !!window?.NebulaShell,
+        isReadonly: !window?.NebulaShell,
     });
     return state;
 };
