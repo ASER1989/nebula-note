7a759af112375bef97a74801a2b2df13348f6ac9
黄旭 <aser1989@sina.com>
2025-05-17 11:23:33 +0800

    update scrollbox

--- nebula-ui/.storybook/main.ts
+++ nebula-ui/.storybook/main.ts
@@ -1,6 +1,9 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import { dirname, join } from 'path';
+import { createRequire } from 'module';
 
+// @ts-ignore
+const require = createRequire(import.meta.url);
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
--- nebula-ui/src/molecules/scrollBox/index.tsx
+++ nebula-ui/src/molecules/scrollBox/index.tsx
@@ -1,23 +1,27 @@
 import './index.styl';
 import React, { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 export type ScrollBoxProps = {
     children: React.ReactNode;
     styleMode?: 'dark' | 'light';
+    step?: number;
 };
 
-const ScrollBox = ({ children, styleMode = 'light' }: ScrollBoxProps) => {
+const ScrollBox = ({ children, styleMode = 'light', step = 200 }: ScrollBoxProps) => {
     const containerRef = useRef<HTMLDivElement | null>(null);
     const contentRef = useRef<HTMLDivElement | null>(null);
-
     const [offsetX, setOffsetX] = useState(0);
     const maxScrollRef = useRef(0);
+    const [isContentOverflow, setIsContentOverflow] = useState(false);
+
     const updateScrollLimits = () => {
         if (containerRef.current && contentRef.current) {
             const containerWidth = containerRef.current.offsetWidth;
             const contentWidth = contentRef.current.scrollWidth;
             maxScrollRef.current = contentWidth - containerWidth;
+            setIsContentOverflow(maxScrollRef.current > 0);
             if (offsetX > maxScrollRef.current) {
                 setOffsetX(Math.max(0, maxScrollRef.current)); // 保证 offset 不超出范围
             }
@@ -52,22 +56,42 @@ const ScrollBox = ({ children, styleMode = 'light' }: ScrollBoxProps) => {
         }
     };
 
-    const contentClass = classNames('nebula-scroll-box', styleMode, {
-        'scrollable-left': offsetX > 0,
-        'scrollable-right': offsetX < maxScrollRef.current,
-    });
+    const translateHandle = (direction: 'prev' | 'next') => {
+        const delta = direction === 'prev' ? step : 0 - step; // 每次移动的距离
+        setOffsetX((prev) => Math.max(0, Math.min(maxScrollRef.current, prev + delta)));
+    };
+
+    const contentClass = classNames('nebula-scroll-box-content', styleMode);
 
     return (
-        <div className={contentClass} ref={containerRef}>
-            <div
-                className='scroll-box-content'
-                ref={contentRef}
-                style={{
-                    transform: `translateX(-${offsetX}px)`,
-                }}
-            >
-                {children}
+        <div className='nebula-scroll-box'>
+            <div className={contentClass} ref={containerRef}>
+                <div
+                    className='scroll-box-content'
+                    ref={contentRef}
+                    style={{
+                        transform: `translateX(-${offsetX}px)`,
+                    }}
+                >
+                    {children}
+                </div>
             </div>
+            {isContentOverflow && (
+                <div
+                    className='scroll-box-nav prev'
+                    onClick={() => translateHandle('prev')}
+                >
+                    <FiChevronLeft size='20' color='#ffffff' />
+                </div>
+            )}
+            {isContentOverflow && (
+                <div
+                    className='scroll-box-nav next'
+                    onClick={() => translateHandle('next')}
+                >
+                    <FiChevronRight size='20' color='#ffffff' />
+                </div>
+            )}
         </div>
     );
 };
