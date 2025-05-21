e32890b541b57ff5d014bf302702a13bc6cc5b01
ASER1989 <aser1989@sina.com>
2025-05-21 09:37:53 +0000

    update vite config

--- nebula-client/vite.config.ts
+++ nebula-client/vite.config.ts
@@ -64,7 +64,6 @@ export default defineConfig(({ command, mode }) => {
                 // },
                 output: {
                     manualChunks(id) {
-                        if (id.includes('@codemirror/lang-')) return 'codemirror-lang';
                         if (id.includes('toast-ui')) return 'toast-ui';
                         if (id.includes('codemirror')) return 'codemirror';
                         if (id.includes('@uiw')) return 'codemirror';
