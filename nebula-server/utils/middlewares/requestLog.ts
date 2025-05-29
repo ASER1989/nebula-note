2ddd6fe5d7b369054708fcafa93a9f612284975d
黄旭 <aser1989@sina.com>
2025-05-29 21:02:28 +0800

    update replicas

--- nebula-server/utils/middlewares/requestLog.ts
+++ nebula-server/utils/middlewares/requestLog.ts
@@ -3,6 +3,6 @@ import { Context, Middleware, Next } from 'koa';
 export const useRequestLog: () => Middleware = () => {
     return async function (ctx: Context, next: Next) {
         await next();
-        console.log(`Received [${new Date()}]:`, ctx.url);
+        console.log(`Received:`, ctx.url);
     };
 };
