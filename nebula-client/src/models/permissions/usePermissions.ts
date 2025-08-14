0f964869acd36a7e8042b200261f673fbee0cb36
aser2025 <aser1989@outlook.com>
2025-08-14 06:30:15 +0000

    reset usePermissions

--- nebula-client/src/models/permissions/usePermissions.ts
+++ nebula-client/src/models/permissions/usePermissions.ts
@@ -4,7 +4,7 @@ import { useRedux } from '@client/store/hooks/useRedux';
 const REDUX_KEY = 'permissions';
 
 // @ts-ignore
-const IS_READONLY = import.meta.env.DEV ? false : false;
+const IS_READONLY = import.meta.env.DEV ? false : undefined;
 export const usePermissions = () => {
     const { state } = useRedux<Permission>(REDUX_KEY, {
         isReadonly: IS_READONLY ?? !window?.NebulaShell,
